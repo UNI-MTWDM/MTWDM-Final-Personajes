c571e08a874e56b5bc0868266b1a99130553e814
José Manuel Rodríguez Morales <z9manolo@gmail.com>
2023-12-02 13:13:59 -0600

    Carga inicial

---
+++ MTWDM-Final-Personajes/MTWDM_Final_PersonajesApp.swift
@@ -0,0 +1,20 @@
+//
+//  MTWDM_Final_PersonajesApp.swift
+//  MTWDM-Final-Personajes
+//
+//  Created by Manuel Rodríguez on 02/12/23.
+//
+
+import SwiftUI
+
+@main
+struct MTWDM_Final_PersonajesApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
